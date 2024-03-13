1df264675b7098a578ed9eefe1d3176aebcec0c3
MariiaTomkiv <mariia.tomkiv@gmail.com>
2024-03-13 13:38:03 +0200

    Task_1

---
+++ Task_1.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+   
+def get_days_from_today():
+    try:
+        user_input = input('Введіть дату у форматі PPPP-MM-ДД: ')
+        user_date = datetime.strptime(user_input, '%Y-%m-%d')
+        current_data = datetime.today()
+        delta_days = current_data - user_date
+        return(delta_days.days)
+    except:
+        print("Введіть дату у правильному форматі!")
+    
+
+print(get_days_from_today())
