7e1e9510c59bb8f96086f761542ddcea9c1bbaff
MariiaTomkiv <mariia.tomkiv@gmail.com>
2024-03-17 15:02:41 +0200

    task 3

---
+++ Task_3.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+users = [
+    {"name": "Mariia Tomkiv", "birthday": "1981.03.17"},
+    {"name": "Jon Silver", "birthday": "1981.03.25"},
+    {"name": "Oksana Gural", "birthday": "1976.01.03"}
+]
+def prepeared_users_birthday():
+        prepared_users = []
+        today = datetime.today().date()
+        for user in users:
+                birthday = datetime.strptime(user["birthday"], "%Y.%m.%d").date()
+                birthday = birthday.replace(year=today.year)
+                if birthday < today:
+                        birthday = birthday.replace(year=today.year+1)
+                        diference = (birthday - today).days
+                        if diference < 7:
+                                week_day = birthday.strftime("%A")
+                                if week_day == "Saturday" or week_day == "Sunday":
+                                        week_day == "Monday"
+                                        congratulation_date = week_day.strftime("%Y.%m.%d")
+                                        prepared_users.append({
+                                                "name": users["name"],
+                                                "birthday": "congratulation_date"
+                                                 })
+                print(prepared_users)
+
+prepeared_users_birthday()
